59048500aa1ab8b4f0529ec11b613973035d74fe
shan2008 <2505517739@qq.com>
2020-02-05 11:43:02 +0800

    Revert "commit two"
    This reverts commit a86608c85dcd699bac32156752e218c0fbf300f4.

--- learningone/learningone-host/src/main/java/com/you/learn/APP.java
+++ learningone/learningone-host/src/main/java/com/you/learn/APP.java
@@ -1,5 +1,7 @@
 package com.you.learn;
 
+import com.ctrip.ibu.platform.shark.sdk.api.Shark;
+import com.ctrip.ibu.platform.shark.sdk.service.SharkInitializer;
 import com.you.learn.entity.DataSource;
 import com.you.learn.entity.UserEntity;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,11 +13,27 @@ import java.util.Optional;
  */
 public class APP {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Springconfig.class);
+       /* AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Springconfig.class);
         DataSource dataSource=applicationContext.getBean(DataSource.class);
         if(dataSource!=null){
 			//test
             System.out.println(dataSource.getPassWord()+ " "+dataSource.getUserName());
+        }*/
+
+
+        try {
+            SharkInitializer.init();
+            Shark.initThreadLocale("en-HK");
+            String a = Shark.get(100024873, "key.test", "en-HK", "fail");
+            System.out.println(a);
+        } catch (Exception e) {
+
+            System.out.println(e);
         }
+
+
+
+
+
     }
 }
